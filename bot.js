9bae3ebd4983b51b3e3b64d6947860804766ca35
tameem12 <47356373+tameem12@users.noreply.github.com>
2020-03-30 02:25:13 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,184 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = "*";
+
+
+client.on('message', message => { // اقتراح
+    if (message.content.startsWith(prefix + 'sug')) {
+        if (message.author.bot) return
+        if (!message.guild) return message.reply('**:x: This Commands Just In Server**').then(v => {v.react('❌')})
+        var args =  message.content.split(' ').slice(1).join(' ')
+        if (!args) return message.reply('Type You Suggestion').then(c => {c.delete(5000)})
+        let Room = message.guild.channels.find(686757669809291409)
+        if (!Room) return message.channel.send("Can't find suggestions channel.").then(d => d.react('❌'))
+        let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setAuthor(`Vote on ${message.author.username}'s suggestion`, message.author.avatarURL)
+        .addField('**Suggestion**',`${args}`)
+        .setThumbnail(message.author.avatarURL)
+        .setFooter(`ID: ${message.author.id}`)
+        Room.sendEmbed(embed).then(c => {
+            c.react('✅').then(() => 
+                c.react('❌'))
+
+        }).catch(e => console.error(e)
+        )
+    }
+});
+
+client.on("message", message => { // تقديم اداره
+  if(message.content.startsWith(prefix + "admin")) {
+        if(!message.channel.guild) return;
+                if(message.author.bot) return;
+        let channel = message.guild.channels.find(686757653036138702)
+            if(!channel) return message.reply("**لانشاء روم التقديمات !!setsubmissions من فضلك اكتب الامر**")
+            if(channel) {
+            message.channel.send( message.member + ', **:timer:**').then( (m) =>{
+              m.edit( message.member + ', **اسمك الحقيقي  ✍**' )
+              m.channel.awaitMessages( m1 => m1.author == message.author,{ maxMatches: 1, time: 60*1000 } ).then ( (m1) => {
+                  m1 = m1.first();
+                  var name = m1.content;
+                  m1.delete();
+                  m.edit(message.member + ', **:timer:**').then( (m) =>{
+                      m.edit( message.member + ', **كم عمرك؟ 🎓**' )
+                      setTimeout(() => {
+                        m.delete()
+                      }, 10000);
+                      m.channel.awaitMessages( m2 => m2.author == message.author,{ maxMatches: 1, time: 60*1000 } ).then ( (m2) => {
+                          m2 = m2.first();
+                          var age = m2.content;
+                          m2.delete()
+                          message.channel.send( message.member + ', **:timer:**').then( (m) =>{
+                            m.edit( message.member + ', **كم ساعه تكون متفاعل بالسيرفر؟ 🕒**' )
+                            setTimeout(() => {
+                              m.delete()
+                            }, 10000);
+                            m.channel.awaitMessages( m1 => m1.author == message.author,{ maxMatches: 1, time: 60*1000 } ).then ( (m3) => {
+                                m3 = m3.first();
+                                var ask = m3.content;
+                                m3.delete();
+                                message.channel.send( message.member + ', **:timer:**').then( (m) =>{
+                                  m.edit( message.member + ', **هل ستحترم القوانين ؟ 📑**' )
+                                  setTimeout(() => {
+                                    m.delete()
+                                  }, 10000);
+                                  m.channel.awaitMessages( m1 => m1.author == message.author,{ maxMatches: 1, time: 60*1000 } ).then ( (m4) => {
+                                      m4 = m4.first();
+                                      var ask2 = m4.content;
+                                      m4.delete();
+                                      message.channel.send( message.member + ', **:timer:**').then( (m) =>{
+                                        m.edit( message.member + ', **ما هي خبراتك 💻 **' )
+                                        m.channel.awaitMessages( m1 => m1.author == message.author,{ maxMatches: 1, time: 60*1000 } ).then ( (m5) => {
+                                            m5 = m5.first();
+                                            var ask3 = m5.content;
+                                            m5.delete();
+                      m.edit(message.member + ', **....جارى جمع البيانات**').then( (mtime)=>{
+                        setTimeout(() => {
+                          let embed = new Discord.RichEmbed()
+                        .setColor('RANDOM')
+                        .setTitle(`**تقديم ادارة** [__**${message.guild.name}**__]`)
+                        .addField('**` ✍ الاسم`**', `${name}` , true)
+                        .addField('**` 🎓 العمر`**', `${age}` , true)
+                        .addField('**` 🕒 ساعات تفاعله`**',`${ask}`)
+                        .addField('**` 📑 هل سيحترم القوانين ؟`**',`${ask2}`)
+                        .addField('**` 💻 خبراتة`**',`${ask3}`)
+                        .setFooter(message.author.username,'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')
+                        channel.send(embed)
+                        }, 2500);
+                        setTimeout(() => {
+                          mtime.delete()
+                        }, 3000);
+
+                  })
+                })
+                })
+              })
+            })
+          })
+        })
+        })
+              })
+          })
+        })
+    }
+}
+        });
+    client.on('message',async message => {
+  let mention = message.mentions.members.first();
+  let mySupport = message.guild.roles.find(686757506743009297);
+  if(message.content.startsWith(prefix + "accept")) {
+    let acRoom = message.guild.channels.find(686757653036138702);
+    if(acRoom) {
+    if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return;
+    if(!mention) return message.reply('منشن شخص');
+
+    mention.addRole(mySupport).then(() => {
+      acRoom.send(`**[ ${myHelper} ] واعطائك رتبة ${mention} تم بنجاح قبولك**`);
+    });
+  }
+}
+});
+
+client.on('message',async message => {
+  let mention = message.mentions.members.first();
+  if(message.content.startsWith(prefix + "reject")) {
+  if(!message.channel.guild) return;
+  let acRoom = message.guild.channels.find(686757653036138702);
+  if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return;
+  if(!mention) return message.reply("منشن شخص");
+ 
+  acRoom.send(`**${mention} تم رفضك للاسف**`)
+  }
+});
+client.on("message", message => {
+    if (message.content === (prefix + "help")) {
+     const embed = new Discord.RichEmbed() 
+         .setColor("#580e6b")
+         .setThumbnail(message.author.avatarURL)
+         .setDescription(`**
+      *sug للإقتراحات 💡
+      *admin للتقديم على الإدارة 👑
+      *accept لقبول طلب التقديم ✅
+      *reject لرفض طلب التقديم  ❌
+       **  `)
+
+   message.author.sendEmbed(embed)
+   
+   }
+   });
+const developers = ["472413769700474901",""]
+var adminprefix = ["*T11440"]
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'setg')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'setw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'setl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'sets')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/dream");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setava')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});   
+
+client.login(process.env.BOT_TOKEN);
